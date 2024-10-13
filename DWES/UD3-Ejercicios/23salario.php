285b441d89c30fe595775e94054cb9a6f7750fbf
Javier Puertas <javierpuertas2004@gmail.com>
2024-10-13 12:39:17 +0200

    [] - crear ejercicios

---
+++ DWES/UD3-Ejercicios/23salario.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @Author: Javier Puertas
+ */
+
+
+// Dado un vector asociativo de trabajadores con su salario creado solicitando al usuario el nombre
+// y salario de cada trabajador, crea usando funciones el salario máximo, el salario mínimo y el
+// salario medio
+
+
+
+?>
---
+++ DWES/UD3-Ejercicios/24aumentosalario.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @Author: Javier Puertas
+ */
+
+// Con los trabajadores del ejercicio anterior, calcular el salario actual y el salario aumentado un
+// porcentaje indicado por la variable
+
+
+?>
