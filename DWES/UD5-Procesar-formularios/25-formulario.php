ee682e668127ee1b0f94a57e875a20295bbc2125
jaavii004 <javierpuertas2004@gmail.com>
2025-01-18 20:53:13 +0100

    Up

--- DWES/UD5-Procesar-formularios/25-formulario.php
+++ DWES/UD5-Procesar-formularios/25-formulario.php
@@ -4,7 +4,16 @@
  * @Author: Javier Puertas
  */
 
-// 25. Crea una Web para obtener los siguientes datos: Nombre completo, Contraseña (mínimo 6 caracteres)...
+// 25. Crea una Web para obtener los siguientes datos: Nombre completo, Contraseña (mínimo 6
+// caracteres), Nivel de Estudios(Sin estudios, Educación Secundaria Obligatoria, Bachillerato,
+// Formación Profesional, Estudios Universitarios), Nacionalidad (Española, Otra), Idiomas
+// (Español, Inglés, Francés, Alemán Italiano), Email, Adjuntar Foto (sólo extensiones jpg, gif y
+// png, tamaño máximo 50 KB). Además de las comprobaciones de validación, se debe comprobar
+// que sube fichero, que el fichero tiene extensión (puedes usar explode()) y ésta es válida, que hay
+// directorio donde guardarlo y que se genera con nombre único. Si todo ha ido bien, redirige al
+// usuario a una página donde se le indique que se ha procesado con éxito e incluye tu nombre y
+// grupo de clase.
+
 
 $errores = [];
 $nombre = $_POST['nombre_completo'] ?? '';
